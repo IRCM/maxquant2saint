2baeaca77dc01ef5780f7a67cad1ca8393c253b4
Christian Poitras <christian.poitras@ircm.qc.ca>
2018-11-30 16:19:18 -0500

    Fixes SequenceService.

--- src/main/java/ca/qc/ircm/maxquant2saint/fasta/SequenceService.java
+++ src/main/java/ca/qc/ircm/maxquant2saint/fasta/SequenceService.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import javax.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SequenceService {
   private static final Logger logger = LoggerFactory.getLogger(SequenceService.class);
+  @Inject
   private FastaConfiguration configuration;
 
   protected SequenceService() {
